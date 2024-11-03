aad996cef9ca9fba0ae2f6dbfcb29771ed35988e
Carolina Bolivar <cdelterror123@gmail.com>
2024-11-03 12:07:16 -0500

    cap-157

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string = 'burbuja';
+  public nameUpper: string = 'bellota';
+  public fullName: string = 'bOmBOm';
 }
