3848785a7602d522a17f1b03629c426e0d3227ad
Carolina Bolivar <cdelterror123@gmail.com>
2024-11-05 16:45:24 -0500

    cap-160

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -7,6 +7,16 @@ import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 
+//configuración del locale de la app
+import localEsCO from '@angular/common/locales/es-CO';
+import localPtPT from '@angular/common/locales/pt-PT';
+
+
+import { registerLocaleData } from '@angular/common';
+
+//se manda a llamar para que establezca en angular los locales
+registerLocaleData(localEsCO);
+registerLocaleData(localPtPT);
 
 @NgModule({
   declarations: [
@@ -19,7 +29,7 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
 ],
   providers: [
-    provideClientHydration()
+    { provide: LOCALE_ID, useValue: 'es-CO' }
   ],
   bootstrap: [AppComponent]
 })
